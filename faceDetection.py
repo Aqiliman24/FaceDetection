c4e6866c2c7efd20a6a018a3e6a985c0eacc877f
medkad24 sdn bhd <aqiliman24@gmail.com>
2023-01-17 10:28:58 +0800

    first initial

---
+++ faceDetection.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, request
+from sklearn.metrics import accuracy_score
+import cv2
+
+app = Flask(__name__)
+
+@app.route("/api/face_detection", methods=["POST"])
+def face_detection():
+    
+    data = request.files.get('image')
+    data.save("data/image.jpg")
+    # Load the cascade classifier
+    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+
+    # Read the input image
+    img = cv2.imread('data/image.jpg')
+    img = cv2.resize(img,(500,400))
+
+    # Convert into grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    if len(faces) > 0: 
+        value=True
+        print ("True")
+    else:
+        value=False
+        print ("False")
+
+
+    # # Draw rectangle around the faces
+    # for (x, y, w, h) in faces:
+    #     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Display the output
+    # cv2.imshow("Faces", img)
+    cv2.waitKey()
+    return jsonify(output=value)
+
+if __name__ == '__main__':
+    app.run(debug=True)
