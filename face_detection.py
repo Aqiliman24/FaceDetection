ba9fce98c088eefc6b612025b2cbdf768fde432c
medkad24 sdn bhd <aqiliman24@gmail.com>
2023-01-30 15:49:37 +0800

    aws failed images update

--- face_detection.py
+++ face_detection.py
@@ -1,8 +1,45 @@
 from flask import Flask, jsonify, request
-# from sklearn.metrics import accuracy_score
 import cv2
+import os
+import boto3
+from dotenv import load_dotenv
+from ulid import ULID
+import datetime
+
 
 app = Flask(__name__)
+
+def connectAWS(imgpath):
+
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID_FACEDETECTION')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY_FACEDETECTION')
+    AWS_BUCKET = os.getenv('AWS_BUCKET_FACEDETECTION')
+    AWS_LOCATION = os.getenv('AWS_LOCATION_FACEDETECTION')
+
+    conn = boto3.Session(AWS_ACCESS_KEY_ID,
+            AWS_SECRET_ACCESS_KEY)
+
+
+    # bucket = conn.get_bucket(AWS_BUCKET)
+    # conn.create_bucket(Bucket=AWS_BUCKET, CreateBucketConfiguration={
+    # 'LocationConstraint': AWS_LOCATION})
+
+    s3 = conn.resource('s3')
+
+    # object = s3.Object(AWS_BUCKET, 'test')
+
+    res = s3.Bucket(AWS_BUCKET).upload_file(imgpath,imgpath,ExtraArgs={ "ContentType": "image/jpeg"})
+    print(res)
+    # response = conn.upload_file(img, AWS_BUCKET)
+
+    # res = response.get('ResponseMetadata')
+
+    if res == ():
+        print('File Uploaded Successfully')
+    else:
+        print('File Not Uploaded')
+
+
 
 @app.route("/landing_page", methods=["GET"])
 def landing_page():
@@ -21,12 +58,13 @@ def landing_page():
 def face_detection():
     
     data = request.files.get('image')
-    data.save("data/image.jpg")
+    imgpath = 'FailedImage/' + str(ULID.from_datetime(datetime.datetime.now()))+'.jpg'
+    data.save(imgpath)
     # Load the cascade classifier
     face_cascade = cv2.CascadeClassifier("model/haarcascade_frontalface_alt2.xml")
 
     # Read the input image
-    img = cv2.imread('data/image.jpg')
+    img = cv2.imread(imgpath)
     img = cv2.resize(img,(500,400))
 
     # Convert into grayscale
@@ -41,9 +79,15 @@ def face_detection():
     else:
         value=False
         print ("False")
+        connectAWS(imgpath)
+    
+    os.remove(imgpath)
+    
 
     cv2.waitKey()
     return jsonify(output=value)
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0',port=8002,debug=True)
+    load_dotenv()
+    test = (os.getenv('PORT_FACEDETECTION'))
+    app.run(host = '0.0.0.0',port=test,debug=True)
